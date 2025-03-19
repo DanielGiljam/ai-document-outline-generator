fa190758a85de08592c7d12234a3053439e81e5c
Daniel Giljam <33825175+DanielGiljam@users.noreply.github.com>
2025-03-19 14:12:45 +0200

    add documents collection

---
+++ src/collections/Documents.ts
@@ -0,0 +1,25 @@
+import { HeadingFeature, lexicalEditor } from '@payloadcms/richtext-lexical'
+import type { CollectionConfig } from 'payload'
+
+export const Documents: CollectionConfig = {
+  slug: 'documents',
+  admin: {
+    useAsTitle: 'name',
+  },
+  fields: [
+    {
+      type: 'text',
+      name: 'name',
+    },
+    {
+      type: 'richText',
+      name: 'content',
+      editor: lexicalEditor({
+        features: ({ rootFeatures }) => [
+          ...rootFeatures,
+          HeadingFeature({ enabledHeadingSizes: ['h2', 'h3', 'h4', 'h5', 'h6'] }),
+        ],
+      }),
+    },
+  ],
+}
