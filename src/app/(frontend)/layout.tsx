f0d57b7260f2a8c74c29dc37479e035b1f580b15
Daniel Giljam <33825175+DanielGiljam@users.noreply.github.com>
2025-03-19 14:20:02 +0200

    add back placeholder index page styles

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -12,7 +12,7 @@ export default async function RootLayout(props: { children: React.ReactNode }) {
 
   return (
     <html lang="en">
-      <body>
+      <body className="dark">
         <main>{children}</main>
       </body>
     </html>
